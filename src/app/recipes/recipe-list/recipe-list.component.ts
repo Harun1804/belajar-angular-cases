42eceac3ca08e192edc6324638c5379cfdfdbf6d
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-11-04 16:02:24 +0700

    Add ui sample

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('Telur Dadar','Enak mantap gurih','https://awsimages.detik.net.id/community/media/visual/2022/02/06/resep-dadar-telur-ala-rumah-makan-padang_43.jpeg?w=1200'),
+    new Recipe('Telur Dadar','Enak mantap gurih','https://awsimages.detik.net.id/community/media/visual/2022/02/06/resep-dadar-telur-ala-rumah-makan-padang_43.jpeg?w=1200')
+  ];
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../model/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Telur', 4),
+    new Ingredient('Bawang Daun', 2),
+  ];
 }
